3dde5cface1affcc4d2f338897214b4008698b28
Uthmaan <samsodienuthmaan@gmail.com>
2023-05-17 10:55:49 +0200

    First Push

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let name =('Uthmaan');
+console.log(typeof name);
+
+name = 4;
+console.log(typeof name);
+
+let you =(true);
+console.log(typeof you);
+
+let me;
+console.log(typeof me);
+
+let us =null;
+console.log(typeof us);
